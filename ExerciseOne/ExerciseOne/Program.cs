a76f36e5b760fb174ccb41c4d09d8d078fe2cf7a
AJ Harrington <r11132a@gmail.com>
2019-10-17 15:30:01 -0400

    Migrated Program.FizzBuzz to swtich statement from an if/else if/else construct  to be fancy.

--- ExerciseOne/ExerciseOne/Program.cs
+++ ExerciseOne/ExerciseOne/Program.cs
@@ -60,14 +60,21 @@ namespace ExerciseOne
         {
             for(int i = 0;i <= 100;++i)
             {
-                if (i % 3 == 0 && i % 15 != 0)
-                    Console.WriteLine("Fizz");
-                else if (i % 5 == 0 && i % 15 != 0)
-                    Console.WriteLine("Buzz");
-                else if (i % 15 == 0)
-                    Console.WriteLine("FizzBuzz");
-                else
-                    Console.WriteLine(i);
+                switch(i)
+                {
+                    case int j when (i % 3 == 0 && i % 15 != 0):
+                        Console.WriteLine("Fizz");
+                        break;
+                    case int j when (i % 5 == 0 && i % 15 != 0):
+                        Console.WriteLine("Buzz");
+                        break;
+                    case int j when i % 15 == 0:
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                    default:
+                        Console.WriteLine(i);
+                        break;
+                }
             }
         }
         #endregion
