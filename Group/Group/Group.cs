94371f297b40e41fa14b2fdab7f681dfa86f89de
AJ Harrington <r11132a@gmail.com>
2019-11-10 14:38:03 -0500

    Added Group class library.  Still need to complete the section on the Loop function, but all other functionality and associated tests have been added.

---
+++ Group/Group/Group.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group
+{
+    public class Group<T>
+    {
+        private T[] _data = null;
+        private int _count = 0;
+
+        private const int DEFAULT_SIZE = 10;
+
+        public Group(int startingSize = DEFAULT_SIZE)
+        {
+            _data = new T[startingSize];
+        }
+
+        public bool Has(T item) => Array.IndexOf<T>(_data, item) == -1 ? false : true;
+
+        public void Add(T item)
+        {
+            if (!Has(item))
+                Insert(item);
+        }
+
+        public void Delete(T item)
+        {
+            int index = Array.IndexOf<T>(_data, item);
+            if (index == -1) return; // Don't have it, just do nothing.
+            if (index == 0) // At the beginning
+            {
+                T[] temp = new T[_count - 1];
+                Array.ConstrainedCopy(_data, 1, temp, 0, _count - 1);
+                _data = temp;
+            }
+            else if (index == _count - 1) // At the end
+            {
+                Array.Resize(ref _data, _count - 1);
+            }
+            else // Somewhere in the middle
+            {
+                T[] temp = new T[_count - 1];
+                Array.ConstrainedCopy(_data, 0, temp, 0, index); // Copy the first part
+                Array.ConstrainedCopy(_data, index + 1, temp, index, _count - index - 1); // Copy the rest
+                _data = temp;
+            }
+            _count--;
+        }
+
+        /// <summary>
+        /// Returns a copy of the underlying array, sized to the count
+        /// </summary>
+        /// <returns>T[]</returns>
+        public T[] ToArray()
+        {
+            T[] temp = new T[_count];
+
+            Array.ConstrainedCopy(_data, 0, temp, 0, _count);
+
+            return temp;
+        }
+
+        public static Group<T> CreateFromCollection(IEnumerable<T> collection)
+        {
+            Group<T> g = new Group<T>();
+            foreach(var item in collection)
+            {
+                g.Add(item);
+            }
+
+            return g;
+        }
+
+
+        private void Insert(T item)
+        {
+            if(++_count > _data.Length)
+            {
+                Array.Resize(ref _data, _data.Length + DEFAULT_SIZE);
+            }
+            _data[_count - 1] = item;
+        }
+    }
+}
---
+++ Group/Group/GroupTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Group
+{
+    public class GroupTest
+    {
+
+        [Fact]
+        public void Test_Add_One()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            int[] expected = { 1 };
+            Assert.Equal(expected, g.ToArray());
+        }
+
+        [Fact]
+        public void Test_Add_Several()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            g.Add(3);
+            g.Add(2);
+            int[] expected = { 1, 3, 2 };
+            Assert.Equal(expected, g.ToArray());
+        }
+
+        [Fact]
+        public void Test_Add_Existing()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            g.Add(1);
+
+            int[] expected = { 1 };
+            Assert.Equal(expected, g.ToArray());
+        }
+
+        [Fact]
+        public void Test_Delete_one()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            g.Delete(1);
+            Assert.False(g.Has(1));
+        }
+
+        [Fact]
+        public void Test_Delete_Front()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            g.Add(2);
+            g.Add(3);
+            g.Delete(1);
+
+            int[] expected = { 2, 3 };
+            Assert.Equal(expected, g.ToArray())
+;
+        }
+
+        [Fact]
+        public void Test_Delete_End()
+        {
+            Group<int> g = new Group<int>();
+            g.Add(1);
+            g.Add(2);
+            g.Add(3);
+
+            g.Delete(3);
+            int[] expected = { 1, 2 };
+
+            Assert.Equal(expected, g.ToArray());
+        }
+
+        [Fact]
+        public void Test_Delete_Middle()
+        {
+            Group<int> g = new Group<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                g.Add(i);
+            }
+
+            g.Delete(3);
+
+            int[] expected = { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
+
+            Assert.Equal(expected, g.ToArray());
+        }
+
+        [Fact]
+        public void Test_CreateFromCollection()
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+
+            Group<int> g = Group<int>.CreateFromCollection(list);
+
+            int[] expected = { 1, 2, 3, 4, 5 };
+            Assert.Equal(expected, g.ToArray());
+        }
+    }
+}
