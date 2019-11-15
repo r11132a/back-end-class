a7433e0a9a96d6f3cdcbbbf1b0b85d2f743247fb
AJ Harrington <r11132a@gmail.com>
2019-11-14 21:01:46 -0500

    Added small console program to demonstrate the Loop.

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopWithInt();
+            LoopWithString();
+
+            Console.ReadLine();
+        }
+
+        public static void LoopWithString()
+        {
+            Loop<string>("a",
+                s => s != "aaaaaaaa",
+                s => Console.WriteLine($"I have received {s}"),
+                s => s + s
+                );
+        }
+
+        public static void LoopWithInt()
+        {
+            int loopEnd = 10;
+
+            Loop<int>(1,
+                i => i <= loopEnd,
+                i => Console.WriteLine($"I got {i}"),
+                i => i + 1);
+        }
+
+        public static void Loop<T>(
+            T value,
+            Func<T,bool> Test,
+            Action<T> Body,
+            Func<T,T> Update)
+        {
+            if(Test(value))
+            {
+                Body(value);
+                Loop(Update(value),Test,Body,Update);
+            }
+            return;
+        }
+            
+    }
+}
